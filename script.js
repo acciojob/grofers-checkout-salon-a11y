f2a784cac081f893a26cb88212e0687d5e40de65
saloni mourya <manshimaurya195@gmail.com>
2026-01-19 18:00:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,25 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+			let prices=document.querySelectorAll(".price")
+            let total = 0;
+	      for(let i = 0;i<prices.length;i++){
+		   total+= Number (prices[i].innerText);
+		  }
+		let table=document.querySelector("table")
+		let newrow = document.createElement("tr")
+		let datatable1 = document.createElement("td")
+	    let datatable2 = document.createElement("td")
+			  
+			  // datatable.innerText = "total prices:"  + total;
+			  // newrow.append(datatable1,datatable2)
+			  // table.append(newrow)
+			  datatable1.innerText = "total prices:"
+			  datatable2.innerText = total;
+	           newrow.append(datatable1,datatable2)
+			   table.append(newrow)
+		
+	
   
 };
 
