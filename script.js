431fae052349378a298b4d4c846f24f088e69041
saloni mourya <manshimaurya195@gmail.com>
2026-01-19 22:13:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-			let prices=document.querySelectorAll(".price")
+			let prices=document.querySelectorAll(".prices")
             let total = 0;
 	      for(let i = 0;i<prices.length;i++){
 		   total+= Number (prices[i].innerText);
